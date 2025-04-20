4a4f0b59f96d227ef69dbc84a05bf9a303075d1a
BEYOND BV <rabebienvenu@gmail.com>
2025-04-20 15:30:51 +0100

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -5,10 +5,10 @@ import os
 app = Flask(__name__)
 
 # Vérifie si les variables d'environnement sont bien définies
-TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
-TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
-TWILIO_PHONE_NUMBER = os.environ.get("TWILIO_PHONE_NUMBER")
-ADMIN_PHONE_NUMBER = os.environ.get("ADMIN_PHONE_NUMBER")  # Ton numéro à toi, ex: +229XXXXXXXX
+TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+TWILIO_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")
+ADMIN_PHONE_NUMBER = os.getenv("ADMIN_PHONE_NUMBER") 
 
 if not all([TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE_NUMBER, ADMIN_PHONE_NUMBER]):
     raise ValueError("Les variables d'environnement de Twilio sont manquantes !")
